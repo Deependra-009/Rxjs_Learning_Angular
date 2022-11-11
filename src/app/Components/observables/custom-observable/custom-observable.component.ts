6787e1d3d79893c78ca4e5fc72ee2a22f15a57c5
Deependra-009 <deependra04trivedi@gmail.com>
2022-11-12 02:37:34 +0530

    Rxjs

--- src/app/Components/observables/custom-observable/custom-observable.component.ts
+++ src/app/Components/observables/custom-observable/custom-observable.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DesignUtilityService } from 'src/app/Services/design-utility.service';
 
 @Component({
   selector: 'app-custom-observable',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomObservableComponent implements OnInit {
 
-  constructor() { }
+  techStatus:any;
+
+  constructor(
+    private _designUtility:DesignUtilityService
+  ) { }
 
   ngOnInit(): void {
+
+    // example 1 (Manual)
+
+    const cusObs1=Observable.create((observer:any)=>{
+
+      setTimeout(()=>{
+        observer.next('Angular');
+      },1000)
+      setTimeout(()=>{
+        observer.next('TypeScript');
+      },2000)
+      setTimeout(()=>{
+        observer.next('React');
+        
+        // observer.error("limit exceed");
+      },3000)
+      setTimeout(()=>{
+        observer.next('HTML/CSS');
+        observer.complete();
+      },4000)
+      
+    })
+
+    cusObs1.subscribe((res:any)=>{
+      this._designUtility.print(res,"elContainer")
+      
+    },
+    ()=>{
+      this.techStatus="error";
+    },()=>{
+      this.techStatus="completed";
+    })
+
+    // subscribe(data,error,completion)
+
+
   }
 
 }
