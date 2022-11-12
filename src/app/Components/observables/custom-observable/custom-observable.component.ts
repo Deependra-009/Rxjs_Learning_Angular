b0f8a18335b2c04bb0542a19fde3b83d4b823ae2
Deependra-009 <deependra04trivedi@gmail.com>
2022-11-12 13:22:38 +0530

    Rxjs

--- src/app/Components/observables/custom-observable/custom-observable.component.ts
+++ src/app/Components/observables/custom-observable/custom-observable.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { DesignUtilityService } from 'src/app/Services/design-utility.service';
 
 @Component({
@@ -7,9 +7,14 @@ import { DesignUtilityService } from 'src/app/Services/design-utility.service';
   templateUrl: './custom-observable.component.html',
   styleUrls: ['./custom-observable.component.css']
 })
-export class CustomObservableComponent implements OnInit {
+export class CustomObservableComponent implements OnInit,OnDestroy {
 
   techStatus:any;
+  techStatus1:any;
+  techStatus2:any;
+  name:any;
+
+  subs2=new Subscription;
 
   constructor(
     private _designUtility:DesignUtilityService
@@ -52,6 +57,71 @@ export class CustomObservableComponent implements OnInit {
     // subscribe(data,error,completion)
 
 
+    // example 2 custom interval
+    const arr2=["Angular","Typescript","React","HTML/CSS"];
+    const cusObs2=Observable.create((observer:any)=>{
+      let count=0;
+      setInterval(()=>{
+        observer.next(arr2[count]);
+        if(count==2){
+          // observer.error("error limit");          
+        }
+        else if(count==3){
+          // observer.complete();
+          observer.error();
+        }
+        count++;
+      },1000);
+      
+    })
+    this.subs2=cusObs2.subscribe((res:any)=>{
+      // console.log(res);
+      this._designUtility.print(res,"elContainer2");
+    },
+    ()=>{
+      this.techStatus1="error";
+    },
+    ()=>{
+      this.techStatus1="completed";
+    })
+
+
+    // example 3 randome names
+    const arr3=['Angular','React','Java','Html','CSS'];
+    const cusObs3=Observable.create((observer:any)=>{
+      let count=0;
+      setInterval(()=>{
+        observer.next(arr3[count++]);
+        if(count==2){
+          // observer.error("error limit");          
+        }
+        else if(count==arr3.length){
+          observer.complete();
+          // observer.error();
+        }
+        
+      },1000);
+
+    })
+
+    cusObs3.subscribe((res:any)=>{
+      console.log(res);
+      this.name=res;
+      
+    },
+    ()=>{
+      this.techStatus2="error";
+    },
+    ()=>{
+      this.techStatus2="completed";
+    })
+
+
+
+  }
+
+  ngOnDestroy(): void {
+    this.subs2.unsubscribe();
   }
 
 }
